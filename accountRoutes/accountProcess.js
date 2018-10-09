7438c626a9ee8b5925803fdb08006c305bb18f13
vinitkumargoel <vinit928@gmail.com>
2018-10-09 21:26:25 +0530

    added baselines for packages

--- accountRoutes/accountProcess.js
+++ accountRoutes/accountProcess.js
@@ -1,7 +1,7 @@
 'use strict';
 /*
-*module dependencies
-*/
+ *module dependencies
+ */
 //var multer = require('multer');
 var express = require("express");
 var fs = require("fs");
@@ -13,47 +13,45 @@ var router = express.Router();
 const logger = require('./../applogger');
 var totalBalances = require('../data/totalBalances');
 
-router.get('/credit',fetchCreditAccounts);
-router.get('/debit',fetchDebitAccount);
+router.get('/credit', fetchCreditAccounts);
+router.get('/debit', fetchDebitAccount);
 router.get('/totalBalances', fetchTotalBalances);
 
-function fetchTotalBalances(req, res, next){
+function fetchTotalBalances(req, res, next) {
     var token = req.headers['x-access-token'];
 
-    jwt.verify(token, config.secret , function(err, decodedObj){
-        if (err)
-        {
+    jwt.verify(token, config.secret, function (err, decodedObj) {
+        if (err) {
             return res.status(500).send({
-                auth: false, message: 'Failed to authenticate token.'
+                auth: false,
+                message: 'Failed to authenticate token.'
             })
         };
         let userName = decodedObj.username;
-        var userObj = totalBalances.userData.filter((obj)=>{
+        var userObj = totalBalances.userData.filter((obj) => {
             return obj.username == userName;
         })[0];
-        if(userObj){
+        if (userObj) {
             res.status(200).json(userObj);
         }
     });
 }
 
-function fetchCreditAccounts(req, res, next){
+function fetchCreditAccounts(req, res, next) {
     var token = req.headers['x-access-token'];
 
-    jwt.verify(token, config.secret , function(err, decodedObj){
-        if (err)
-        {
+    jwt.verify(token, config.secret, function (err, decodedObj) {        
+        if (err) {
             return res.status(401).send({
-                auth: false, message: 'Failed to authenticate token.'
+                auth: false,
+                message: 'Failed to authenticate token.'
             })
         };
         let userName = decodedObj.username;
         let credit;
-        try{
-            credit = require('../data/credit/'+userName);
-        }
-        catch(err)
-        {
+        try {
+            credit = require('../data/credit/' + userName);
+        } catch (err) {
             res.status(500).json({
                 errorMsg: 'User data not available'
             });
@@ -63,21 +61,20 @@ function fetchCreditAccounts(req, res, next){
 
 }
 
-function fetchDebitAccount(req, res){
+function fetchDebitAccount(req, res) {
     var token = req.headers['x-access-token'];
-    jwt.verify(token, config.secret, function(err, decodedObj){
-        if (err){
+    jwt.verify(token, config.secret, function (err, decodedObj) {
+        if (err) {
             return res.status(401).send({
-                auth: false, message: 'Failed to authenticate token.'
+                auth: false,
+                message: 'Failed to authenticate token.'
             });
         }
         let userName = decodedObj.username;
         let debit;
-        try{
-            debit = require('../data/debit/'+userName)
-        }
-        catch(err)
-        {
+        try {
+            debit = require('../data/debit/' + userName)
+        } catch (err) {
             res.status(500).json({
                 errorMsg: "User data not available"
             });
@@ -86,24 +83,24 @@ function fetchDebitAccount(req, res){
     });
 }
 
-function getCreditPath(userName){
+function getCreditPath(userName) {
     // let path = "../data/credit/"+userName+".json";
     // let credit = require(path);
-    let fullpath = path.join(__dirname,"..","/data/credit/",userName+".json");
+    let fullpath = path.join(__dirname, "..", "/data/credit/", userName + ".json");
     //let fullpath = "/data/credit/"+userName+".json";
     let rawdata = fs.readFileSync(fullpath);
     let credit = JSON.parse(rawdata);
-    return  credit;
+    return credit;
 }
 
 
-function getDebitPath(userName){
+function getDebitPath(userName) {
     // let fullpath = "../data/debit/"+userName+".json";
     // let debit = require(fullpath);
-    let fullpath = path.join(__dirname,"..","/data/debit/",userName+".json");
+    let fullpath = path.join(__dirname, "..", "/data/debit/", userName + ".json");
     let rawdata = fs.readFileSync(fullpath);
     let debit = JSON.parse(rawdata);
-    return  debit;
+    return debit;
 }
 
 module.exports = router;
--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 const assert = require('chai').assert;
 const request = require('supertest');
 
-const BASE_URL = process.env.NODE_ENV == "PRO" ? "/accountsService/" : "/"
 
 const alice_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsaWNlIiwiaWF0IjoxNTM4OTk2MjQ4LCJleHAiOjE1MzkwODI2NDh9._eaVntxHE99wVQIDYMQ0ICUDrJusetscYoKGU0tZPfk";
 const john_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFsaWNlIiwiaWF0IjoxNTM4OTk2MjQ4LCJleHAiOjE1MzkwODI2NDh9._eaVntxHE99wVQIDYMQ0ICUDrJusetscYoKGU0tZPfk";
@@ -13,58 +12,58 @@ describe('Account service', function () {
   describe('Alice Debit account', function () {
     xit('Calling the alice debit account - positive', function (done) {
       container
-        .get(`${BASE_URL}debit`)
+        .get(`/accountsService/debit`)
         .set("x-access-token", alice_token)
         .expect(200, done)
     });
     it('Calling the alice debit account with wrong token - negative', function (done) {
       container
-        .get(`${BASE_URL}debit`)
+        .get(`/accountsService/debit`)
         .set("x-access-token", "wrong token")
         .expect(401, done)
     });
   })
 
-  xdescribe('Alice credit account', function () {
+  describe('Alice credit account', function () {
     xit('Calling the alice credit account - positive', function (done) {
       container
-        .get(`${BASE_URL}/credit`)
+        .get(`/accountsService/credit`)
         .set("x-access-token", alice_token)
         .expect(200, done)
     });
     it('Calling the alice credit account with wrong token - negative', function (done) {
       container
-        .get(`${BASE_URL}/credit`)
+        .get(`/accountsService/credit`)
         .set("x-access-token", "wrong token")
         .expect(401, done)
     });
   })
 
-  xdescribe('John Debit account', function () {
+  describe('John Debit account', function () {
     xit('Calling the john debit account - positive', function (done) {
       container
-        .get(`${BASE_URL}/debit`)
+        .get(`/accountsService/debit`)
         .set("x-access-token", john_token)
         .expect(200, done)
     });
     it('Calling the john debit account with wrong token - negative', function (done) {
       container
-        .get(`${BASE_URL}/debit`)
+        .get(`/accountsService/debit`)
         .set("x-access-token", "wrong token")
         .expect(401, done)
     });
   })
 
-  xdescribe('john credit account', function () {
+  describe('john credit account', function () {
     xit('Calling the john credit account - positive', function (done) {
       container
-        .get(`${BASE_URL}/credit`)
+        .get(`/accountsService/credit`)
         .set("x-access-token", john_token)
         .expect(200, done)
     });
     it('Calling the john credit account with wrong token - negative', function (done) {
       container
-        .get(`${BASE_URL}/credit`)
+        .get(`/accountsService/credit`)
         .set("x-access-token", "wrong token")
         .expect(401, done)
     });
